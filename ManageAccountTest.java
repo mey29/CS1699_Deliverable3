bd2df6f3a42ee4a2d4c0328d351fcbd15e5ce16d
fisherjoshua9 <fisherjoshua9@gmail.com>
2015-03-02 23:56:45 -0500

    Update Files

--- ManageAccountTest.java
+++ ManageAccountTest.java
@@ -1,24 +1,16 @@
 /*
  * User Story 3:
- * Feature:
+ * 
  * As a user of Amazon I want to manage my account so that I can have up-to-date information and I am aware of account balances. 
  * 
  * Scenarios:
  * 
- * 1. Add a new address
- * 		Given a signed in Amazon account page, when I click "Add New Address," then a new address can be constructed.
- * 2. Change name
- * 		Given a signed in Amazon account page, when I click "Change Account Settings," then my name can be updated.
- * 3. View product availability alerts
- * 		Given a signed in Amazon account page, when I click "Product Availability Alerts," then all alerts associated with the account will be listed.
- * 4. View past orders
- * 		Given a signed in Amazon account Wish List page, when I click "View Archived Orders," then I see a list of order history.
- * 5. View gift card balance
- * 		Given a signed in Amazon account page, when I click "View Gift Card Balance," then my current gift card balance is displayed.
- * 6. View payment options
- * 		Given a signed in Amazon account page, when I click "Manage Payment Options," then a list of payment methods for my account is displayed.
- * 7. View browsing history
- * 		Given a signed in Amazon account page, when I click "View and Edit Your Browsing History," then all recently viewed items appear.
+ *  Given a signed in Amazon account page, when I click "Return or replace items," then past ordered items may be replaced. * 	Given a signed in Amazon account page, when I click "Change Account Settings," then my name can be updated.
+ * 	Given a signed in Amazon account page, when I click "Product Availability Alerts," then all alerts associated with the account will be listed.
+ * 	Given a signed in Amazon account page, when I click "View Archived Orders," then I see a list of order history.
+ * 	Given a signed in Amazon account page, when I click "View Gift Card Balance," then my current gift card balance is displayed.
+ * 	Given a signed in Amazon account page, when I click "Manage Payment Options," then a list of payment methods for my account is displayed.
+ * 	Given a signed in Amazon account page, when I click "View and Edit Your Browsing History," then all recently viewed items appear.
  */
 
 import static org.junit.Assert.*;
@@ -29,17 +21,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class ManageAccount {
+public class ManageAccountTest {
 
 	// Create new WebDriver
 	WebDriver driver = new FirefoxDriver();
 
 	public void setUp() throws Exception {
-		
+
 		// Base URL of WebDriver at amazon.com
 		driver.get("https://amazon.com");
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 		// Travel to sign in page
 		driver.findElement(By.id("nav-signin-text")).click();
 
@@ -48,40 +40,39 @@ public class ManageAccount {
 				"softwaretesting1699@gmail.com");
 		driver.findElement(By.id("ap_password")).sendKeys("testingcs");
 		driver.findElement(By.id("signInSubmit-input")).click();
-		driver.findElement(By.cssSelector("#nav-your-account > span.nav-button-title.nav-button-line2")).click();
+		driver.findElement(
+				By.cssSelector("#nav-your-account > span.nav-button-title.nav-button-line2"))
+				.click();
 	}
 
-	/*
-	 * This test case checks that a successful message is received upon updating
-	 * the account name.
-	 */
+	// Given a signed in Amazon account page, when I click
+	// "Return or replace items," then past ordered items may be replaced.
 	@Test
-	public void testNameChange() {
-		
-		// Click on "Change Account Settings"
-		driver.findElement(By.linkText("Change Account Settings")).click();
+	public void testReturnItems() {
+		try {
+			setUp();
+		} catch (Exception e) {
+			fail();
+		}
 
-		// Click on change name
-		driver.findElement(By.id("cnep_1A_change_name_button-input")).click();
-
-		// Enter new name
-		driver.findElement(By.id("ap_customer_name")).sendKeys(
-				"SoftwareUpgrade");
-		driver.findElement(By.id("cnep_1C_submit_button-input")).click();
+		// Click on "Return or replace items"
+		driver.findElement(By.linkText("Return or replace items")).click();
 
 		// Test Case
-		WebElement element = driver.findElement(By.id("message_success"));
-		assertNotNull(element);
+		assertTrue(driver.getPageSource().contains("Returns Center"));
 		driver.quit();
 	}
 
-	/*
-	 * This test case checks that a successful message is received upon 
-	 * adding a new address.
-	 */
+	// Given a signed in Amazon account page, when I click
+	// "Change Account Settings," then my name can be updated.
 	@Test
 	public void testAddAddress() {
-		
+		try {
+			setUp();
+		} catch (Exception e) {
+			fail();
+		}
+
 		// Click on "Add New Address"
 		driver.findElement(By.linkText("Add New Address")).click();
 
@@ -90,7 +81,8 @@ public class ManageAccount {
 		driver.findElement(By.id("enterAddressAddressLine1")).sendKeys(
 				"4200 Fifth Avenue");
 		driver.findElement(By.id("enterAddressCity")).sendKeys("Pittsburgh");
-		driver.findElement(By.id("enterAddressState")).sendKeys("Pennsylvania");
+		driver.findElement(By.id("enterAddressStateOrRegion")).sendKeys(
+				"Pennsylvania");
 		driver.findElement(By.id("enterAddressPostalCode")).sendKeys("15260");
 		driver.findElement(By.id("enterAddressPhoneNumber")).sendKeys(
 				"4126244141");
@@ -102,64 +94,105 @@ public class ManageAccount {
 		driver.quit();
 	}
 
-	/*
-	 * This test case checks that no alerts exist for the account.
-	 */
+	// Given a signed in Amazon account page, when I click
+	// "Product Availability Alerts," then all alerts associated with the
+	// account will be listed.
 	@Test
 	public void testAvailabilityAlerts() {
-		
+		try {
+			setUp();
+		} catch (Exception e) {
+			fail();
+		}
+
 		// Click on "Product Availability Alerts"
 		driver.findElement(By.linkText("Product Availability Alerts")).click();
-		
-		// Test Case
-		assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("You do not have any alerts."));
 
+		// Test Case
+		assertTrue(driver.findElement(By.className("small")).getText()
+				.matches("You do not have any alerts."));
 		driver.quit();
 	}
 
+	// Given a signed in Amazon account page, when I click
+	// "View Archived Orders," then I see a list of order history.
 	@Test
 	public void testViewOrders() {
-		
+		try {
+			setUp();
+		} catch (Exception e) {
+			fail();
+		}
+
 		// Click on "View Archived Orders"
 		driver.findElement(By.linkText("View Archived Orders")).click();
-		
-		// Test Case
-		assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("There are no archived orders in your account"));
 
+		// Test Case
+		assertTrue(driver
+				.findElement(By.id("ordersContainer"))
+				.getText()
+				.matches(
+						"There are no archived orders in your account. View all orders"));
 		driver.quit();
 	}
 
+	// Given a signed in Amazon account page, when I click
+	// "View Gift Card Balance," then my current gift card balance is displayed.
 	@Test
 	public void testViewGiftCardBalance() {
-		
+		try {
+			setUp();
+		} catch (Exception e) {
+			fail();
+		}
+
 		// Click on "View Gift Card Balance"
 		driver.findElement(By.linkText("View Gift Card Balance")).click();
-	
+
 		// Test Case
-		assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("$0.00"));
+		assertTrue(driver.getPageSource().contains("$0.00"));
 		driver.quit();
 	}
 
-//	@Test
-//	public void testPaymentOptions() {
-//		// Click on "Add New Address"
-//		driver.findElement(By.linkText("Add New Address")).click();
-//
-//		//HERE
-//
-//		// Test Case
-//		WebElement element = driver.findElement(By.id("myab-alert-bar-title"));
-//		assertNotNull(element);
-//		driver.quit();
-//	}
+	// Given a signed in Amazon account page, when I click
+	// "Manage Payment Options," then a list of payment methods for my account
+	// is displayed.
+	@Test
+	public void testPaymentOptions() {
+		try {
+			setUp();
+		} catch (Exception e) {
+			fail();
+		}
+		// Click on "Manage Payment Options"
+		driver.findElement(By.linkText("Manage Payment Options")).click();
 
+		// Test Case
+		assertTrue(driver
+				.findElement(By.id("subHeaderLeftCol"))
+				.getText()
+				.matches(
+						"Here are the payment methods stored in your Amazon account."));
+	}
+
+	// Given a signed in Amazon account page, when I click
+	// "View and Edit Your Browsing History," then all recently viewed items
+	// appear.
 	@Test
 	public void testBrowsingHistory() {
+		try {
+			setUp();
+		} catch (Exception e) {
+			fail();
+		}
+
 		// Click on "View and Edit Your Browsing History"
-		driver.findElement(By.linkText("View and Edit Your Browsing History")).click();
-		
+		driver.findElement(By.linkText("View and Edit Your Browsing History"))
+				.click();
+
 		// Test Case
-		assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("Moby Dick"));
+		assertTrue(driver.getPageSource().contains(
+				"You have no recently viewed items to display."));
 		driver.quit();
 	}
 }
--- SignInTest.java
+++ SignInTest.java
@@ -1,21 +1,18 @@
 /*
  * User Story 2:
- * Feature:
+ * 
  * As a user of Amazon I want to be able to sign in so that I can make purchases. 
  * 
  * Scenarios:
  * 
- * 1. Correct username and correct password
- * 		Given the Amazon sign-in page, when I enter the correct username and password, then I am signed into my account.
- * 2. Incorrect username and incorrect password
- * 		Given the Amazon sign-in page, when I enter the incorrect username and password, then I am not signed into my account and receive an error message.
- * 3. Incorrect username and correct password
- * 		Given the Amazon sign-in page, when I enter the incorrect username and correct password, then I am not signed into my account and receive an error message.
- * 4. Correct username and incorrect password
- *   	Given the Amazon sign-in page, when I enter the correct username and incorrect password, then I am not signed into my account and receive an error message.
+ * 	Given the Amazon sign-in page, when I enter the correct username and password, then I am signed into my account.
+ * 	Given the Amazon sign-in page, when I enter the incorrect username and password, then I am not signed into my account and receive an error message.
+ * 	Given the Amazon sign-in page, when I enter the incorrect username and correct password, then I am not signed into my account and receive an error message.
+ *  Given the Amazon sign-in page, when I enter the correct username and incorrect password, then I am not signed into my account and receive an error message.
  */
 
 import static org.junit.Assert.*;
+
 import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.openqa.selenium.*;
@@ -23,22 +20,20 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SignIn {
+public class SignInTest {
 
 	/*
-	 * This test assures that with both the correct username and password Sign
-	 * in is achieved. 
-	 * Username email: softwaretesting1699@gmail.com 
-	 * Password: testingcs
+	 * Given the Amazon sign-in page, when I enter the correct username and
+	 * password, then I am signed into my account.
 	 */
 	@Test
 	public void testCorrectInput() {
-		
-		//Create new driver with base URL at amazon.com
+
+		// Create new driver with base URL at amazon.com
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://amazon.com");
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 		// Travel to sign-in page
 		driver.findElement(By.id("nav-your-account")).click();
 
@@ -47,59 +42,61 @@ public class SignIn {
 				"softwaretesting1699@gmail.com");
 
 		// Click on text box for password and enter correct password
-		driver.findElement(By.name("password")).sendKeys("testingcs");
+		driver.findElement(By.id("ap_password")).sendKeys("testingcs");
 
 		// Click "Sign in using our secure server"
-		driver.findElement(By.name("signInSubmit-input")).click();
+		driver.findElement(By.id("signInSubmit-input")).click();
 
 		// Test Case - signed in
-		WebElement element = driver.findElement(By.id("ys-top"));
-		assertEquals(element.getText(), "Your Amazon.com");
+		WebElement element = driver.findElement(By.id("nav-your-amazon"));
+		assertEquals(element.getText(), "Software's Amazon.com");
 		driver.quit();
 	}
-	
+
 	/*
-	 * This test assures that with both incorrect username and password Sign
-	 * in is NOT achieved. 
+	 * Given the Amazon sign-in page, when I enter the incorrect username and
+	 * password, then I am not signed into my account and receive an error
+	 * message.
 	 */
 	@Test
 	public void testIncorrectOutput() {
-		
-		//Create new driver with base URL at amazon.com
+
+		// Create new driver with base URL at amazon.com
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://amazon.com");
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 		// Travel to sign in page
 		driver.findElement(By.id("nav-your-account")).click();
 
 		// Click on text box for email address and enter correct email
 		driver.findElement(By.id("ap_email")).sendKeys("1699@gmail.com");
-		
+
 		// Click on text box for password and enter correct password
 		driver.findElement(By.id("ap_password")).sendKeys("test");
-		
+
 		// Click "Sign in using our secure server"
 		driver.findElement(By.id("signInSubmit-input")).click();
 
 		// Test Case - NOT signed in
-		WebElement element = driver.findElement(By.id("message_error"));
-		assertEquals(element.getText(), "There was a problem with your request");
+		WebElement element = driver.findElement(By.id("message_warning"));
+		assertNotNull(element);
 		driver.quit();
 	}
 
 	/*
-	 * This test assures that with both incorrect username and correct password Sign
-	 * in is NOT achieved. 
+	 * Given the Amazon sign-in page, when I enter the incorrect username and
+	 * correct password, then I am not signed into my account and receive an
+	 * error message.
 	 */
 	@Test
 	public void testWrongUsername() {
-		
-		//Create new driver with base URL at amazon.com
+
+		// Create new driver with base URL at amazon.com
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://amazon.com");
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 		// Travel to sign in page
 		driver.findElement(By.id("nav-your-account")).click();
 
@@ -113,23 +110,24 @@ public class SignIn {
 		driver.findElement(By.id("signInSubmit-input")).click();
 
 		// Test Case - NOT signed in
-		WebElement element = driver.findElement(By.id("message_error"));
-		assertEquals(element.getText(), "There was a problem with your request");
+		WebElement element = driver.findElement(By.id("message_warning"));
+		assertNotNull(element);
 		driver.quit();
 	}
 
 	/*
-	 * This test assures that with both correct username, but incorrect password Sign
-	 * in is NOT achieved. 
+	 * Given the Amazon sign-in page, when I enter the correct username and
+	 * incorrect password, then I am not signed into my account and receive an
+	 * error message.
 	 */
 	@Test
 	public void testWrongPassword() {
-		
-		//Create new driver with base URL at amazon.com
+
+		// Create new driver with base URL at amazon.com
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://amazon.com");
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 		// Travel to sign in page
 		driver.findElement(By.id("nav-your-account")).click();
 
@@ -143,8 +141,8 @@ public class SignIn {
 		driver.findElement(By.id("signInSubmit-input")).click();
 
 		// Test Case - NOT signed in
-		WebElement element = driver.findElement(By.id("message_error"));
-		assertEquals(element.getText(), "There was a problem with your request");
+		WebElement element = driver.findElement(By.id("message_warning"));
+		assertNotNull(element);
 		driver.quit();
 	}
 }
